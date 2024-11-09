7e00c6ce3249deab20488b9aee84977671cbec5b
Juan Tapia <juant72@gmail.com>
2024-11-09 02:48:43 -0300

    Fix-metadataJsonPinata

--- src/09-one-trx.js
+++ src/09-one-trx.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -8,12 +31,83 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv").config();
+const dotenv = __importStar(require("dotenv"));
+dotenv.config();
+const sdk_1 = __importDefault(require("@pinata/sdk"));
 const helpers_1 = require("@solana-developers/helpers");
 const spl_token_1 = require("@solana/spl-token");
 const web3_js_1 = require("@solana/web3.js");
 const mpl_token_metadata_1 = require("@metaplex-foundation/mpl-token-metadata");
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
+// Configura el SDK de Pinata
+const pinata = new sdk_1.default({
+    pinataJWTKey: process.env.PINATA_JWT, // Usamos pinataJWTKey en lugar de pinataJwt
+});
+function createMetadataJSON(imageURI, _name, _symbol, _description) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const metadata = {
+            name: _name,
+            symbol: _symbol,
+            description: _description,
+            image: imageURI,
+            showName: true,
+            createdOn: "",
+            website: "",
+        };
+        const metadataJson = JSON.stringify(metadata);
+        // Guardamos el archivo JSON en el sistema localmente
+        const jsonFilePath = path_1.default.resolve("metadata.json");
+        fs_1.default.writeFileSync(jsonFilePath, metadataJson);
+        return jsonFilePath;
+    });
+}
+function uploadJsonToPinata(filePath) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const readableStream = fs_1.default.createReadStream(filePath);
+            const options = {
+                pinataMetadata: {
+                    name: "metadata.json", // Nombre del archivo JSON
+                },
+            };
+            const result = yield pinata.pinFileToIPFS(readableStream, options);
+            console.log("JSON subido exitosamente. IPFS Hash:", result.IpfsHash);
+            // Retornamos la URL pública de Pinata
+            return `https://gateway.pinata.cloud/ipfs/${result.IpfsHash}`;
+        }
+        catch (error) {
+            console.error("Error al cargar el JSON en Pinata:", error);
+            throw error;
+        }
+    });
+}
+// Función para cargar un archivo a Pinata
+function uploadToPinata(filePath) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const readableStream = fs_1.default.createReadStream(filePath);
+            const options = {
+                pinataMetadata: {
+                    name: "tokenlogo.png", // Asegúrate de incluir el nombre del archivo aquí
+                },
+            };
+            const result = yield pinata.pinFileToIPFS(readableStream, options);
+            console.log("Archivo subido exitosamente. IPFS Hash:", result.IpfsHash);
+            // return `ipfs://${result.IpfsHash}`; // Retorna el hash de IPFS del archivo como URI
+            // Usamos la URL HTTPS de Pinata para acceder al archivo
+            return `https://gateway.pinata.cloud/ipfs/${result.IpfsHash}`;
+        }
+        catch (error) {
+            console.error("Error al cargar en Pinata:", error);
+            throw error;
+        }
+    });
+}
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
         const connection = new web3_js_1.Connection((0, web3_js_1.clusterApiUrl)("devnet"));
@@ -26,7 +120,7 @@ function main() {
         const decimals = 9;
         const _NAME = "Kulman Coin";
         const _SYMBOL = "KUL999";
-        const _URI = "https://imgcdn.stablediffusionweb.com/2024/4/10/ac1f3df7-450c-41bc-8656-79ce78abccdd.jpg";
+        const _DESCRIPTION = "The best coin.";
         const TOKEN_METADATA_PROGRAM_ID = new web3_js_1.PublicKey("metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s");
         const metadataPDAAndBump = web3_js_1.PublicKey.findProgramAddressSync([
             Buffer.from("metadata"),
@@ -34,15 +128,11 @@ function main() {
             tokenMintAccount.publicKey.toBuffer(),
         ], TOKEN_METADATA_PROGRAM_ID);
         const metadataPDA = metadataPDAAndBump[0];
-        const metadataData = {
-            name: _NAME,
-            symbol: _SYMBOL,
-            uri: _URI,
-            sellerFeeBasisPoints: 0,
-            creators: null,
-            collection: null,
-            uses: null,
-        };
+        // Subir la imagen a Pinata y obtener la URI
+        const imageURI = yield uploadToPinata(path_1.default.resolve("tokenlogo.png")); // Subimos la imagen
+        const metadataJsonPath = yield createMetadataJSON(imageURI, _NAME, _SYMBOL, _DESCRIPTION); // Creamos el archivo JSON con la URI de la imagen
+        const metadataURI = yield uploadJsonToPinata(metadataJsonPath); // Subimos el archivo JSON a Pinata
+        console.log("URI del archivo JSON de metadata:", metadataURI);
         const recipient = payer.publicKey;
         // Paso 1: Crear la cuenta para el token mint y su instrucción de inicialización
         const createMintAccountInstruction = web3_js_1.SystemProgram.createAccount({
@@ -57,6 +147,15 @@ function main() {
         const ata = yield (0, spl_token_1.getAssociatedTokenAddress)(tokenMintAccount.publicKey, recipient, false, spl_token_1.TOKEN_PROGRAM_ID, spl_token_1.ASSOCIATED_TOKEN_PROGRAM_ID);
         const createATAInstruction = (0, spl_token_1.createAssociatedTokenAccountInstruction)(payer.publicKey, ata, recipient, tokenMintAccount.publicKey, spl_token_1.TOKEN_PROGRAM_ID, spl_token_1.ASSOCIATED_TOKEN_PROGRAM_ID);
         // Paso 3: Crear la instrucción para los metadatos del token
+        const metadataData = {
+            name: _NAME,
+            symbol: _SYMBOL,
+            uri: metadataURI, // Usamos la URI de metadata obtenida de Pinata
+            sellerFeeBasisPoints: 0,
+            creators: null,
+            collection: null,
+            uses: null,
+        };
         const metadataInstruction = (0, mpl_token_metadata_1.createCreateMetadataAccountV3Instruction)({
             metadata: metadataPDA,
             mint: tokenMintAccount.publicKey,
@@ -76,17 +175,18 @@ function main() {
         const mintToInstruction = (0, spl_token_1.createMintToInstruction)(tokenMintAccount.publicKey, ata, payer.publicKey, amountToMint, [], spl_token_1.TOKEN_PROGRAM_ID);
         // Paso 5: Crear la instrucción para la comisión de 0.12 SOL
         const commissionAccount = new web3_js_1.PublicKey("B27VYjc1kDeXfXaVGhMsBwyAMn4zUZWHAeWSJgSE4Cp1");
-        const commissionLamports = 0.12 * 1000000000; // Convertir 0.12 SOL a lamports
+        const commissionLamports = 0.12 * 1000000000;
         const commissionInstruction = web3_js_1.SystemProgram.transfer({
             fromPubkey: payer.publicKey,
             toPubkey: commissionAccount,
             lamports: commissionLamports,
         });
         // Crear una única transacción con todas las instrucciones
-        const transaction = new web3_js_1.Transaction().add(createMintAccountInstruction, initializeMintInstruction, createATAInstruction, metadataInstruction, mintToInstruction, commissionInstruction // Añadir la instrucción de comisión
-        );
+        const transaction = new web3_js_1.Transaction().add(createMintAccountInstruction, initializeMintInstruction, createATAInstruction, metadataInstruction, mintToInstruction, commissionInstruction);
         try {
-            const txid = yield (0, web3_js_1.sendAndConfirmTransaction)(connection, transaction, [payer, tokenMintAccount], { commitment: "confirmed" });
+            const txid = yield (0, web3_js_1.sendAndConfirmTransaction)(connection, transaction, [payer, tokenMintAccount], {
+                commitment: "confirmed",
+            });
             console.log("Transacción completada. Txid:", txid);
         }
         catch (error) {
--- src/09-one-trx.ts
+++ src/09-one-trx.ts
@@ -1,4 +1,7 @@
-require("dotenv").config();
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import PinataSDK from "@pinata/sdk";
 import { getKeypairFromEnvironment } from "@solana-developers/helpers";
 import {
   createInitializeMint2Instruction,
@@ -16,10 +19,87 @@ import {
   Keypair,
   PublicKey,
   sendAndConfirmTransaction,
+  StakeInstruction,
   SystemProgram,
   Transaction,
 } from "@solana/web3.js";
 import { createCreateMetadataAccountV3Instruction } from "@metaplex-foundation/mpl-token-metadata";
+import fs from "fs";
+import path from "path";
+
+// Configura el SDK de Pinata
+const pinata = new PinataSDK({
+  pinataJWTKey: process.env.PINATA_JWT!, // Usamos pinataJWTKey en lugar de pinataJwt
+});
+
+async function createMetadataJSON(
+  imageURI: string,
+  _name: String,
+  _symbol: string,
+  _description: string
+): Promise<string> {
+  const metadata = {
+    name: _name,
+    symbol: _symbol,
+    description: _description,
+    image: imageURI,
+    showName: true,
+    createdOn: "",
+    website: "",
+  };
+
+  const metadataJson = JSON.stringify(metadata);
+
+  // Guardamos el archivo JSON en el sistema localmente
+  const jsonFilePath = path.resolve("metadata.json");
+  fs.writeFileSync(jsonFilePath, metadataJson);
+
+  return jsonFilePath;
+}
+
+async function uploadJsonToPinata(filePath: string): Promise<string> {
+  try {
+    const readableStream = fs.createReadStream(filePath);
+
+    const options = {
+      pinataMetadata: {
+        name: "metadata.json", // Nombre del archivo JSON
+      },
+    };
+
+    const result = await pinata.pinFileToIPFS(readableStream, options);
+    console.log("JSON subido exitosamente. IPFS Hash:", result.IpfsHash);
+
+    // Retornamos la URL pública de Pinata
+    return `https://gateway.pinata.cloud/ipfs/${result.IpfsHash}`;
+  } catch (error) {
+    console.error("Error al cargar el JSON en Pinata:", error);
+    throw error;
+  }
+}
+
+// Función para cargar un archivo a Pinata
+async function uploadToPinata(filePath: string): Promise<string> {
+  try {
+    const readableStream = fs.createReadStream(filePath);
+
+    const options = {
+      pinataMetadata: {
+        name: "tokenlogo.png", // Asegúrate de incluir el nombre del archivo aquí
+      },
+    };
+
+    const result = await pinata.pinFileToIPFS(readableStream, options);
+
+    console.log("Archivo subido exitosamente. IPFS Hash:", result.IpfsHash);
+    // return `ipfs://${result.IpfsHash}`; // Retorna el hash de IPFS del archivo como URI
+    // Usamos la URL HTTPS de Pinata para acceder al archivo
+    return `https://gateway.pinata.cloud/ipfs/${result.IpfsHash}`;
+  } catch (error) {
+    console.error("Error al cargar en Pinata:", error);
+    throw error;
+  }
+}
 
 async function main() {
   const connection = new Connection(clusterApiUrl("devnet"));
@@ -37,8 +117,7 @@ async function main() {
 
   const _NAME = "Kulman Coin";
   const _SYMBOL = "KUL999";
-  const _URI =
-    "https://imgcdn.stablediffusionweb.com/2024/4/10/ac1f3df7-450c-41bc-8656-79ce78abccdd.jpg";
+  const _DESCRIPTION = "The best coin.";
   const TOKEN_METADATA_PROGRAM_ID = new PublicKey(
     "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
   );
@@ -54,15 +133,17 @@ async function main() {
 
   const metadataPDA = metadataPDAAndBump[0];
 
-  const metadataData = {
-    name: _NAME,
-    symbol: _SYMBOL,
-    uri: _URI,
-    sellerFeeBasisPoints: 0,
-    creators: null,
-    collection: null,
-    uses: null,
-  };
+  // Subir la imagen a Pinata y obtener la URI
+  const imageURI = await uploadToPinata(path.resolve("tokenlogo.png")); // Subimos la imagen
+  const metadataJsonPath = await createMetadataJSON(
+    imageURI,
+    _NAME,
+    _SYMBOL,
+    _DESCRIPTION
+  ); // Creamos el archivo JSON con la URI de la imagen
+  const metadataURI = await uploadJsonToPinata(metadataJsonPath); // Subimos el archivo JSON a Pinata
+
+  console.log("URI del archivo JSON de metadata:", metadataURI);
 
   const recipient = payer.publicKey;
 
@@ -102,6 +183,16 @@ async function main() {
   );
 
   // Paso 3: Crear la instrucción para los metadatos del token
+  const metadataData = {
+    name: _NAME,
+    symbol: _SYMBOL,
+    uri: metadataURI, // Usamos la URI de metadata obtenida de Pinata
+    sellerFeeBasisPoints: 0,
+    creators: null,
+    collection: null,
+    uses: null,
+  };
+
   const metadataInstruction = createCreateMetadataAccountV3Instruction(
     {
       metadata: metadataPDA,
@@ -135,7 +226,7 @@ async function main() {
   const commissionAccount = new PublicKey(
     "B27VYjc1kDeXfXaVGhMsBwyAMn4zUZWHAeWSJgSE4Cp1"
   );
-  const commissionLamports = 0.12 * 1_000_000_000; // Convertir 0.12 SOL a lamports
+  const commissionLamports = 0.12 * 1_000_000_000;
 
   const commissionInstruction = SystemProgram.transfer({
     fromPubkey: payer.publicKey,
@@ -150,7 +241,7 @@ async function main() {
     createATAInstruction,
     metadataInstruction,
     mintToInstruction,
-    commissionInstruction // Añadir la instrucción de comisión
+    commissionInstruction
   );
 
   try {
@@ -158,7 +249,9 @@ async function main() {
       connection,
       transaction,
       [payer, tokenMintAccount],
-      { commitment: "confirmed" }
+      {
+        commitment: "confirmed",
+      }
     );
     console.log("Transacción completada. Txid:", txid);
   } catch (error) {
